3e4c90a549b39af317d223f640dfedbd8ccc6c08
Amaan Chaudhary <chaudharyamaan982@gmail.com>
2026-02-04 14:49:18 +0530

    Tut 27 -> BgChanger 04 project initial setup

---
+++ bgChanger04/App.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>Bg Changer</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
