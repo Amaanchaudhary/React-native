865b34f911f80fdacb186c62835dbd9f9ef7af7b
Amaan Chaudhary <chaudharyamaan982@gmail.com>
2026-02-05 12:19:13 +0530

    Tut 31 - Completed Haptic Feedback Working

--- rollTheDice05/src/App.tsx
+++ rollTheDice05/src/App.tsx
@@ -1,10 +1,84 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ImageSourcePropType, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import type { JSX, PropsWithChildren } from 'react'
+import DiceOne from "../assets/One.png"
+import DiceTwo from "../assets/Two.png"
+import DiceThree from "../assets/Three.png"
+import DiceFour from "../assets/Four.png"
+import DiceFive from "../assets/Five.png"
+import DiceSix from "../assets/Six.png"
+import { trigger } from "react-native-haptic-feedback";
+
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType
+}>
+
+// Optional configuration
+const options = {
+  enableVibrateFallback: true,
+  ignoreAndroidSystemSettings: true,
+};
+
+const Dice = ({ imageUrl }: DiceProps): JSX.Element => {
+  return (
+    <View style={styles.diceContainer}>
+      <Image
+        style={styles.diceImage}
+        source={imageUrl}
+      />
+    </View>
+  )
+}
 
 export default function App() {
+  const [diceImage, setDiceImage] = React.useState<ImageSourcePropType>(DiceOne);
+
+  const rollDice = () => {
+
+    // Trigger haptic feedback
+    trigger("impactHeavy", options);
+
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+    switch (randomNumber) {
+      case 1:
+        setDiceImage(DiceOne);
+        break;
+
+      case 2:
+        setDiceImage(DiceTwo);
+        break;
+
+      case 3:
+        setDiceImage(DiceThree);
+        break;
+
+      case 4:
+        setDiceImage(DiceFour);
+        break;
+
+      case 5:
+        setDiceImage(DiceFive);
+        break;
+
+      case 6:
+        setDiceImage(DiceSix);
+        break;
+
+      default:
+        setDiceImage(DiceOne);
+        break
+    }
+
+  }
+
   return (
-    <View>
-      <Text>Roll the Dice</Text>
+    <View style={styles.container}>
+      <Dice imageUrl={diceImage} />
+      <Pressable onPress={rollDice}>
+        <Text style={styles.rollDiceBtnText} accessibilityLabel="Roll The Dice Button">
+          Roll The Dice
+        </Text>
+      </Pressable>
     </View>
   )
 }
