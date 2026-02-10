23ea18ba344813307ca41a4d1ce37fcbd3a6273d
Amaan Chaudhary <chaudharyamaan982@gmail.com>
2026-02-10 10:07:26 +0530

    Tut 37 - Initial setup of TicTacToe07 Project

---
+++ TicTacToe07/App.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>Tic Tac Toe 07</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
