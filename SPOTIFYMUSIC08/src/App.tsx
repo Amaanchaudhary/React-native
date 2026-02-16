22843ba2d5f2cec21585eab7f4e6527b6240ad51
Amaan Chaudhary <chaudharyamaan982@gmail.com>
2026-02-16 11:07:21 +0530

    Tut-41 SPOTIFYMUSIC App Project 08 Initial Setup

---
+++ SPOTIFYMUSIC08/src/App.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>SPotify Clone 08</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
